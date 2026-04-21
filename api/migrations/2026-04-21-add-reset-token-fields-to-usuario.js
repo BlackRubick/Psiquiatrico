86ce2efaa8290fbc67df052f20e731c24f783c65
Cesar Gomez Aguilera <blackrubick@MacBook-Air-de-Cesar.local>
2026-04-21 17:27:49 -0600

    reset2

---
+++ api/migrations/2026-04-21-add-reset-token-fields-to-usuario.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("usuarios", "reset_token", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("usuarios", "reset_token_expiry", {
+      type: Sequelize.DATE,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("usuarios", "reset_token");
+    await queryInterface.removeColumn("usuarios", "reset_token_expiry");
+  },
+};
